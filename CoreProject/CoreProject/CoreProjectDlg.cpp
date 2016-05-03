214bd55331a4f638be32043d814c2c35d94e3a96
Dmitry Rumiantsev <dr270797@gmail.com>
2016-05-03 16:48:05 +0300

    Implemented basic functionality(sans graphics)

--- CoreProject/CoreProject/CoreProjectDlg.cpp
+++ CoreProject/CoreProject/CoreProjectDlg.cpp
@@ -6,12 +6,11 @@
 #include "CoreProject.h"
 #include "CoreProjectDlg.h"
 #include "afxdialogex.h"
+#include "ParamDialog.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
-
-
 // CAboutDlg dialog used for App About
 
 class CAboutDlg : public CDialogEx
@@ -47,21 +46,28 @@ END_MESSAGE_MAP()
 
 
 
-CCoreProjectDlg::CCoreProjectDlg(CWnd* pParent /*=NULL*/)
+CCoreProjectDlg::CCoreProjectDlg(rulesEngine* newEngine,CWnd* pParent /*=NULL*/)
 	: CDialogEx(CCoreProjectDlg::IDD, pParent)
+	, isShort(false)
 {
+	engine = newEngine;
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
 
 void CCoreProjectDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST2, listCtrl);
 }
 
 BEGIN_MESSAGE_MAP(CCoreProjectDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_COMMAND(ID_32780, &CCoreProjectDlg::OnLoadData)
+	ON_COMMAND_RANGE(7000, 8000, &CCoreProjectDlg::OnChooseRule)
+	ON_BN_CLICKED(IDOK, &CCoreProjectDlg::OnBnClickedOk)
+	ON_BN_CLICKED(IDBACK, &CCoreProjectDlg::OnBnClickedBack)
 END_MESSAGE_MAP()
 
 
@@ -74,28 +80,56 @@ BOOL CCoreProjectDlg::OnInitDialog()
 	// Add "About..." menu item to system menu.
 
 	// IDM_ABOUTBOX must be in the system command range.
+
 	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
 	ASSERT(IDM_ABOUTBOX < 0xF000);
 
 	CMenu* pSysMenu = GetSystemMenu(IDR_MENU1);
-	if (pSysMenu != NULL)
+
+	std::unordered_map<std::string, std::vector<std::string>> rulesMap = engine->get_rules_map();
+	
+	auto it = rulesMap.begin();
+	int idInner = 7000;
+	while (it != rulesMap.end())
 	{
-		BOOL bNameValid;
-		CString strAboutMenu;
-		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
-		ASSERT(bNameValid);
-		if (!strAboutMenu.IsEmpty())
+		CMenu *menu = new CMenu;
+		menu->CreatePopupMenu();
+		auto it2 = it->second.begin();
+		while (it2 != it->second.end())
 		{
-			pSysMenu->AppendMenu(MF_SEPARATOR);
-			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+			CString menuItem;
+			menuItem.Format(_T("%S"), it2->c_str());
+			menu->AppendMenu(MF_STRING, idInner, menuItem);
+			it2++;
+			idInner++;
 		}
+		CString popupName;
+		popupName.Format(_T("%S"), it->first.c_str());
+		this->GetMenu()->AppendMenu(MF_POPUP, (UINT_PTR)menu->m_hMenu,popupName);
+		it++;
 	}
 
+	for (int i = 0; i < storage.size(); i++)
+	{
+		listCtrl.AddString(storage[i]);
+	}
+	if (isShort)
+	{
+		CWnd* temp = this->GetDlgItem(IDBACK);
+		temp->EnableWindow(true);
+	}
 	// Set the icon for this dialog.  The framework does this automatically
 	//  when the application's main window is not a dialog
 	SetIcon(m_hIcon, TRUE);			// Set big icon
 	SetIcon(m_hIcon, FALSE);		// Set small icon
-
+	if (isShort)
+	{
+		this->SetWindowTextW(_T("Shortened testing"));
+	}
+	else
+	{
+		this->SetWindowTextW(_T("Boosted testing"));
+	}
 	// TODO: Add extra initialization here
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
@@ -150,3 +184,108 @@ HCURSOR CCoreProjectDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CCoreProjectDlg::OnLoadData()
+{
+	TCHAR szFilters[] = _T("(*.txt)|*.txt");
+	CFileDialog fileDlg(TRUE, _T("txt"), _T("*.txt"),
+		OFN_FILEMUSTEXIST | OFN_HIDEREADONLY, szFilters);
+
+	if (fileDlg.DoModal() == IDOK)
+	{
+		CT2CA pszConvertedAnsiString(fileDlg.GetPathName());
+		std::string pathName(pszConvertedAnsiString);
+
+		diagramData tempData;
+
+		engine->load_start_data(tempData, pathName);
+	
+		//GetSystemMenu(TRUE);
+	}
+	// TODO: Add your command handler code here
+}
+
+
+void CCoreProjectDlg::OnChooseRule(UINT nID)
+{
+	CString menuItem,subMenu;
+	std::string rulesGroup,ruleName;
+	this->GetMenu()->GetMenuString(nID, menuItem, MF_BYCOMMAND);
+	std::unordered_map<std::string, std::vector<std::string>> rulesMap = engine->get_rules_map();
+
+	auto it = rulesMap.begin();
+	while (it != rulesMap.end())
+	{
+		auto it2 = it->second.begin();
+		while (it2 != it->second.end())
+		{
+			CString tempItem;
+			tempItem.Format(_T("%S"), it2->c_str());
+			if (tempItem == menuItem)
+			{
+				subMenu.Format(_T("%S"), it->first.c_str());
+				rulesGroup = it->first;
+				ruleName = *(it2);
+				break;
+			}
+			it2++;
+		}
+		it++;
+	}
+	CParamDialog dial(subMenu);
+	if (dial.DoModal() == IDOK)
+	{
+		std::vector<double> parameters;
+		parameters.push_back(dial.firstParam);
+		if (subMenu == "Two parametrs")
+		{
+			parameters.push_back(dial.secondParam);
+		}
+		diagramData tempData;
+		engine->create_new_rule(tempData, rulesGroup, ruleName, parameters);
+		usedRules.push_back(tempData); 
+		CString result = menuItem + _T(": ");
+		std::vector<std::string> paramNames;
+		engine->get_rule_param(paramNames, rulesGroup, ruleName);
+		for (int i = 0; i < parameters.size(); i++)
+		{
+			CString temp1,temp2;
+			temp1.Format(_T("%f"), parameters[i]);
+			temp2.Format(_T("%S"), paramNames[i].c_str());
+			result += temp2+"="+temp1+_T(" ");
+		}
+		listCtrl.AddString(result);
+		selectedRule.first = rulesGroup;
+		selectedRule.second = ruleName;
+	}
+	
+}
+
+
+void CCoreProjectDlg::OnBnClickedOk()
+{
+	// TODO: Add your control notification handler code here
+	if (listCtrl.GetCurSel() < 0)
+	{
+		MessageBox(_T("Please, select rule in the list in order to proceed"), _T("Error"),
+			MB_ICONERROR | MB_OK);
+	}
+	else{
+		storage.clear();
+		for (int i = 0; i < listCtrl.GetCount(); i++)
+		{
+			CString temp;
+			listCtrl.GetText(i, temp);
+			storage.push_back(temp);
+		}
+		CDialogEx::OnOK();
+	}
+}
+
+
+void CCoreProjectDlg::OnBnClickedBack()
+{
+	this->EndDialog(IDBACK);
+	// TODO: Add your control notification handler code here
+}
--- CoreProject/CoreProject/CoreProjectDlg.h
+++ CoreProject/CoreProject/CoreProjectDlg.h
@@ -4,13 +4,20 @@
 
 #pragma once
 
-
+#include<fstream>
+#include <vector>
+#include <string>
+#include "RulesEngine.h"
+#include "abstractRule.h"
+#include "lineRule.h"
+#include "hyperbolaRule.h"
+#include "afxwin.h"
 // CCoreProjectDlg dialog
 class CCoreProjectDlg : public CDialogEx
 {
 // Construction
 public:
-	CCoreProjectDlg(CWnd* pParent = NULL);	// standard constructor
+	CCoreProjectDlg(rulesEngine* engine,CWnd* pParent = NULL);	// standard constructor
 
 // Dialog Data
 	enum { IDD = IDD_COREPROJECT_DIALOG };
@@ -29,4 +36,23 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLoadData();
+	void SetEngine(rulesEngine* newEngine)
+	{
+		engine = newEngine;
+	}
+private:
+	rulesEngine* engine;
+public:
+	std::vector<diagramData> usedRules;
+private:
+	void OnChooseRule(UINT nID);
+public:
+	CListBox listCtrl;
+	afx_msg void OnBnClickedOk();
+	std::pair<std::string, std::string> selectedRule;
+	afx_msg void OnBnClickedBack();
+	std::vector<CString> storage;
+	bool isShort;
 };
